52451c07d4e7409283979481529d1acd34dcf36e
Edwards <oedwards@travelers.com>
2019-02-11 18:54:37 -0500

    Added to do functionality

--- pages/index.vue
+++ pages/index.vue
@@ -1,93 +1,62 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-xs-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            flat
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <div>
+    <h1> Vue To Do </h1>
+    <v-form @submit.prevent="addToDo">
+      <label for="to-do">
+        To Do
+      </label>
+      <v-text-field id="new-todo" v-model="newToDo" />
+      <v-btn type="submit" id="addButton">
+        Add Item
+      </v-btn>
+    </v-form>
+    <v-list :light=true v-if="toDoList.length >0" two-line>
+      <template v-for="(item, index) in toDoList">
+        <v-list-tile :key="item.title" @click="removeFromTodo(index)">
+          <v-list-tile-content>
+            <v-checkbox v-model="item.complete" />
+            <v-list-tile-title>
+              {{ item.title }}
+            </v-list-tile-title>
+            <!-- <v-btn @click="deleteToDo(item)">
+              Remove
+            </v-btn> -->
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-divider v-if="toDoList.length > 1" :key="index" />
+      </template>
+    </v-list>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
   components: {
-    Logo,
-    VuetifyLogo
+  },
+  data() {
+    return {
+      newToDo: '',
+      toDoList: []
+    }
+  },
+  methods: {
+    addToDo() {
+      this.toDoList.push({
+        title: this.newToDo,
+        complete: false
+      })
+    },
+    removeFromTodo(index) {
+      this.toDoList = this.toDoList.filter((item, idx) => {
+        return idx !== index
+      })
+    },
+    deleteToDo(toDo) {
+      const idx = this.toDoList.indexOf(toDo)
+      this.toDoList.splice(idx, 1)
+    }
   }
 }
 </script>
+<style>
+</style>
